5e4b622794f1194e71ef57436d653be41a7cdd02
two-dimensional-array <max.mazyr2012@gmail.com>
2021-07-12 21:47:34 +0300

    Module Database is optimized. Optimized finding users in json file

--- tb_weather/Database.py
+++ tb_weather/Database.py
@@ -3,45 +3,44 @@ import json
 class JSON:
     def __init__(self, path = "users.json"):
         self.path = path
-        with open(self.path, "w+", encoding="utf-8") as json_file:
-            if json_file.read():
+        with open(self.path, "r+", encoding="utf-8") as json_file:
+            try:
                 self.database = json.load(json_file)
-            else:
+            except:
                 self.database = {'users': []}
                 json.dump(self.database,json_file,indent=4, ensure_ascii=False)
 
     def user_is_find(self,user_id):
         for item in self.database["users"]:
             if item["id"] == user_id:
-                return True
-        return False
+                return item
+        return None
         
     def init_user(self,user_id):
-        if not self.user_is_find(user_id):
+        if self.user_is_find(user_id) == None:
             with open(self.path, "w", encoding="utf-8") as json_file:
-                item = {
-                    "id": int(user_id),
-                    "geolocation": None
-                }
-                self.database["users"].append(item)
+                self.database["users"].append({"id": int(user_id), "geolocation": None})
                 json.dump(self.database,json_file,indent=4, ensure_ascii=False)
         else: pass
 
     def set_geolocation(self,user_id,geolocation):
-        self.init_user(user_id)
+        item = self.user_is_find(user_id)
         with open(self.path, "w", encoding="utf-8") as json_file:
-            for item in self.database["users"]:
-                if item["id"] == int(user_id):
-                    item["geolocation"] = geolocation
-                    json.dump(self.database,json_file,indent=4, ensure_ascii=False)
-                    break
-                else: pass
+            if item:
+                item["geolocation"] = geolocation
+            else:
+                self.database["users"].append({"id": int(user_id), "geolocation": geolocation})
+            json.dump(self.database,json_file,indent=4, ensure_ascii=False)
 
     def get_geolocation(self,user_id):
-        self.init_user(user_id)
-        for item in self.database["users"]:
-            if item["id"] == int(user_id):
-                return item["geolocation"] if item["geolocation"] != None else "Город не задан"
+        item = self.user_is_find(user_id)
+        if item:
+            return item["geolocation"] if item["geolocation"] != None else "Город не задан"
+        else:
+            with open(self.path, "w", encoding="utf-8") as json_file:
+                self.database["users"].append({"id": int(user_id), "geolocation": None})
+                json.dump(self.database,json_file,indent=4, ensure_ascii=False)
+            return "Город не задан"
 
     def update_database(self):
         with open(self.path, "r+", encoding="utf-8") as json_file:
