a49c4daefd0a9555f1cf1e86e013a343ae424c83
two-dimensional-array <max.mazyr2012@gmail.com>
2021-07-13 18:20:56 +0300

    Class CSV is added in module Database, which implement saving users data in csv file

--- tb_weather/tbweather.py
+++ tb_weather/tbweather.py
@@ -13,7 +13,7 @@ help_msg = "Команды:\n/place - Ввод названия города. \n
 keyboard = telebot.types.ReplyKeyboardMarkup(True, True)
 keyboard.row('/place', '/update','/current_place','/help')
 
-db = Database.JSON()
+db = Database.CSV("users.csv",";")
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
