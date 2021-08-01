4ea2911ed6634050ef4b98e1fc5b19b30d3db4ec
two-dimensional-array <max.mazyr2012@gmail.com>
2021-08-01 12:14:03 +0300

    Output help message is added in bash script

--- start.sh
+++ start.sh
@@ -3,17 +3,15 @@
 SCRIPT_PATH=${0%/*}
 PROGRAMM_DIR=tb_weather
 PROGRAMM_NAME=tbweather.py
-PROGRAMM_PATH=$SCRIPT_PATH/$PROGRAMM_DIR/
 CLEAN_NAME=__pycache__
 REQUIRETMENTS_NAME=requiretments.txt
-REQUIRETMENTS_PATH=$SCRIPT_PATH/$REQUIRETMENTS_NAME
 PY=python
 PIP=pip
-ACTION=${1:---install}
+ACTION=$1
 
 run ()
 {
-cd $PROGRAMM_PATH
+cd $SCRIPT_PATH/$PROGRAMM_DIR/
 $PY $PROGRAMM_NAME
 }
 clean ()
@@ -22,7 +20,17 @@ rm -rf $PROGRAMM_PATH/$CLEAN_NAME
 }
 requiretments ()
 {
-$PIP install -r $REQUIRETMENTS_PATH
+$PIP install -r $SCRIPT_PATH/$REQUIRETMENTS_NAME
+}
+help ()
+{
+echo "    ~/start.sh <parameters>"
+echo "-r |--run           - Runing telegram weather bot."
+echo "-c |--clean         - Remove byte-compiled files by python."
+echo "-rq|--requiretments - Install packages for telegram weather bot."
+echo "-i |--install       - Install and run telegram weather bot."
+echo "-rb|--re-build      - Re-building telegram weather bot."
+echo "-h |--help          - Output help message."
 }
 
 case $ACTION in
@@ -42,6 +50,13 @@ run
 -rb|--re-build)
 clean
 run
+;;
+-h|--help)
+help
+;;
+*)
+echo "Enter incorrect action"
+help
 ;;
 esac
 
