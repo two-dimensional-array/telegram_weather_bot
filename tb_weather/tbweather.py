d92870e4a8660d3fcfb878cfd7cfe2f4630c7714
two-dimensional-array <max.mazyr2012@gmail.com>
2021-08-03 10:46:57 +0300

    HTML style messages is implemented

--- tb_weather/tbweather.py
+++ tb_weather/tbweather.py
@@ -20,9 +20,14 @@ COMMANDS = {
     "help": ("/help", BUTTON_HELP_TEXT),
     "cancel":("/cancel", BUTTON_CANCEL_TEXT)
 }
-HELP_MESSAGE = "Команды:\n/place - Ввод названия города. \n/update - Обновление информации об погоде в текущем городе \n/current_place - Вывод текущего города. \n/cancel - Отмена действия. \n/help - Вывод справки по командам бота"
+HELP_MESSAGE  = "<b>Команды:</b>\n"\
+                "<b>/place</b> - Ввод названия города.\n"\
+                "<b>/update</b> - Обновление информации об погоде в текущем городе.\n"\
+                "<b>/current_place</b> - Вывод текущего города.\n"\
+                "<b>/cancel</b> - Отмена действия.\n"\
+                "<b>/help</b> - Вывод справки по командам бота."
 
-bot = TeleBot(telegram_key)
+bot = TeleBot(telegram_key, parse_mode="html")
 config_wether_manager = config.get_default_config()
 config_wether_manager['language'] = 'ru'
 mgr = OWM(weather_key, config=config_wether_manager).weather_manager()
@@ -49,16 +54,19 @@ def main(message: Message) -> None:
 
 def start_message(chat) -> None:
     db.init_user(chat.id)
-    bot.send_message(chat.id, f'Здравствуйте {chat.username} !\nВас приветсвует телеграм бот созданый творцом two-dimensional-array\nДля отображения списка комманд введите /help', reply_markup=control_keyboard)
+    bot.send_message(chat.id, f'Здравствуйте <i>{chat.username}</i> !\n'\
+                               'Вас приветсвует телеграм бот созданый творцом <i>two-dimensional-array</i>\n'\
+                               'Для отображения списка комманд введите <b>/help</b>', reply_markup=control_keyboard)
 
 def set_place(user_id: int) -> None:
-    bot.send_message(user_id, "Введите название вашего города:", reply_markup=cancel_keyboard)
+    bot.send_message(user_id, "Введите название <i>вашего города</i>:", reply_markup=cancel_keyboard)
     bot.register_next_step_handler_by_chat_id(user_id, request_place)
 
 def update_weather(user_id: int) -> None:
     observation = get_current_weather(db.get_geolocation(user_id))
     if observation == None:
-        bot.send_message(user_id, "Не верно введён город!\nВведите название вашего города:", reply_markup=cancel_keyboard)
+        bot.send_message(user_id, "Не верно введён <i>город</i>!\n"\
+                                  "Введите название <i>вашего города</i>:", reply_markup=cancel_keyboard)
         bot.register_next_step_handler_by_chat_id(user_id, request_place)
     else:
         bot.send_message(user_id, output_data(observation), reply_markup=control_keyboard)
@@ -75,22 +83,21 @@ def request_place(message: Message) -> None:
     else:
         observation = get_current_weather(message.text)
         if observation == None:
-            bot.send_message(message.chat.id, "Не верно введён город!\nВведите название вашего города:", reply_markup=cancel_keyboard)
+            bot.send_message(message.chat.id, "Не верно введён <i>город</i>!\n"\
+                                              "Введите название <i>вашего города</i>:", reply_markup=cancel_keyboard)
             bot.register_next_step_handler_by_chat_id(message.chat.id, request_place)
         else:
             db.set_geolocation(message.chat.id, f'{observation.location.name},{observation.location.country}')
             bot.send_message(message.chat.id, output_data(observation), reply_markup=control_keyboard)
 
 def output_data(observation: Observation) -> str:
-    weather = observation.weather
-    answear  = f'В городе {observation.location.name} {weather.detailed_status}\n'
-    answear += f'Температура воздуха составляет {weather.temperature("celsius")["temp"]} °C\n'
-    answear += f'Влажность воздуха равняется {weather.humidity} %\n'
-    answear += f'Скорость ветра состовляет {weather.wind()["speed"]} м/с\n'
-    answear += f'Облачность равняется {weather.clouds} %\n'
-    answear += f'Восход солнца  в {weather.sunrise_time(timeformat="date").time()} по Гринвичу\n'
-    answear += f'Заход солнца в {weather.sunset_time(timeformat="date").time()} по Гринвичу\n'
-    return answear
+    return f'В городе <i>{observation.location.name}</i> <b>{observation.weather.detailed_status}</b>\n'\
+           f'Температура воздуха составляет <b>{observation.weather.temperature("celsius")["temp"]} °C</b>\n'\
+           f'Влажность воздуха равняется <b>{observation.weather.humidity} %</b>\n'\
+           f'Скорость ветра состовляет <b>{observation.weather.wind()["speed"]} м/с</b>\n'\
+           f'Облачность равняется <b>{observation.weather.clouds} %</b>\n'\
+           f'Восход солнца  в <b>{observation.weather.sunrise_time(timeformat="date").time()}</b> по Гринвичу\n'\
+           f'Заход солнца в <b>{observation.weather.sunset_time(timeformat="date").time()}</b> по Гринвичу\n'
 
 def get_current_weather(place: str) -> Observation or None:
     if place == None or len(place) > MAX_LEN_OF_PLACE:
